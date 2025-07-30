6a9df97b1cd8671adc30c11cf446a7cee24c8288
Madi <nmdbzk@gmail.com>
2025-07-30 16:36:20 +0500

    Update ipv6.c

--- src/ipv6.c
+++ src/ipv6.c
@@ -2,7 +2,25 @@
 
 #include <stdint.h>
 
-struct ipv6_t
+typedef struct ipv6_s
 {
   uint8_t value[16];
-};
+} ipv6_t;
+
+char* ipv6_to_str(ipv6_t* ipv6_p, char* str_p)
+{
+    assert(ipv6_p);
+
+    if (!str_p) str_p = (char*)malloc();
+
+    return str_p;
+}
+
+ipv6_t* str_to_ipv6(const char* str_p, ipv6_t* ipv6_p)
+{
+    assert(str_p);
+
+    if (!ipv6_p) ipv6_p = (ipv6_t*)malloc(sizeof(ipv6_t));
+
+    return ipv6_p;
+}
